c7828cd9a023e95779340171f476b19e9ada39d4
sheepMEEMEE <meemee11033@gmail.com>
2024-07-03 11:08:37 +0900

    コミット

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Text healthText;
+
+    public int maxSkill = 10;
+    public int currentSkill;
+    public Text skillText;
+
+    public Text textGameOver;
+
+    private void Start()
+    {
+        currentHealth = maxHealth; //ゲーム開始時にライフを初期化
+        currentSkill = maxSkill;
+        UpdateUI(); //ライフUIの更新
+        textGameOver.enabled = false;
+    }
+
+    public void Raduce(int raduce)
+    {
+        //ライフを減らす
+        currentHealth -= raduce;
+        currentSkill -= raduce;
+        UpdateUI();
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    //ライフUI
+    void UpdateUI()
+    {
+        healthText.text = "Health:" + currentHealth.ToString();
+        skillText.text = "Skill:" + currentSkill.ToString();
+    }
+
+    void Die()
+    {
+        textGameOver.enabled = true;
+    }
+}
